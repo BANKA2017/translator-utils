7f78ce164d456a4aeff694fde3a4991d797058b5
BANKA2017 <kdwnil@hotmail.com>
2023-03-07 00:51:43 +0800

    fix: type of function args

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,16 @@
 
 //global content
-export function Translator(text: string, target: string, platform: string, raw: boolean): Promise<{content: string | unknown, message: string}>
+export function Translator(text: string | string[], target: string, platform: string, raw: boolean): Promise<{content: string | unknown, message: string}>
 
 //translator content
-export function BaiduTranslator(text: string, target: string, raw: boolean): Promise<string | unknown>
-export function DeepL(text: string, target: string, raw: boolean): Promise<string | unknown>
-export function GoogleTranslate(text: string, target: string, raw: boolean): Promise<string | unknown>
-export function GoogleBrowserTranslate(text: string, target: string, raw: boolean): Promise<string | unknown>
-export function MicrosoftTranslator(text: string, target: string, raw: boolean): Promise<string | unknown>
-export function MicrosoftBrowserTranslator(text: string, target: string, raw: boolean): Promise<string | unknown>
-export function SogouBrowserTranslator(text: string, target: string, raw: boolean): Promise<string | unknown>
-export function YandexBrowserTranslator(text: string, target: string, raw: boolean): Promise<string | unknown>
+export function BaiduTranslator(text: string | string[], target: string, raw: boolean): Promise<string | unknown>
+export function DeepL(text: string | string[], target: string, raw: boolean): Promise<string | unknown>
+export function GoogleTranslate(text: string | string[], target: string, raw: boolean): Promise<string | unknown>
+export function GoogleBrowserTranslate(text: string | string[], target: string, raw: boolean): Promise<string | unknown>
+export function MicrosoftTranslator(text: string | string[], target: string, raw: boolean): Promise<string | unknown>
+export function MicrosoftBrowserTranslator(text: string | string[], target: string, raw: boolean): Promise<string | unknown>
+export function SogouBrowserTranslator(text: string | string[], target: string, raw: boolean): Promise<string | unknown>
+export function YandexBrowserTranslator(text: string | string[], target: string, raw: boolean): Promise<string | unknown>
 
 //language list type
 export type LanguageList = {[p in string]: string}
